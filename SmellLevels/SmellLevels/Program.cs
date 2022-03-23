74bd81232f8e7bd59b7aa344c6ff86489ed0eb40
kesutraB <99909890+kesutraB@users.noreply.github.com>
2022-03-23 12:26:43 +0100

    smradoch2

--- SmellLevels/SmellLevels/Program.cs
+++ SmellLevels/SmellLevels/Program.cs
@@ -1,43 +1,99 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace SmellLevels
 {
     internal class Program
     {
+        enum Smells
+        {
+            OnionRings,
+            HorseAss,
+            HoboToughLife,
+            NoSmell,
+        }
+
         static void Main(string[] args)
         {
-            int i, sum = 0;
-            double avg = 0;
-            string name;
+            Dictionary<string, Smells> table = new Dictionary<string, Smells>();
 
+            string name = GetName();
+            double avg = GetAvg(name);
+            Smells level = GetSmell(avg);
+            switch (level)
+            {
+                case Smells.NoSmell:
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine(Texts.NoSmell);
+                }
+                    break;
+
+                case Smells.OnionRings:
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkGreen;
+                    Console.WriteLine(Texts.OnionRings);
+                }
+                    break;
+
+                case Smells.HorseAss:
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                    Console.WriteLine(Texts.HorseAss);
+                }
+                    break;
+
+                case Smells.HoboToughLife:
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(Texts.HoboToughLife);
+                }
+                    break;
+            }
+            Console.ResetColor();
+        }
+
+        private static string GetName()
+        {
+            string name;
             Console.WriteLine("Zadej jméno: ");
             name = Console.ReadLine();
-            for(i = 0; i < name.Length; i++)
+            return name;
+        }
+
+        private static double GetAvg(string name)
+        {
+            int i, sum = 0;
+            double avg;
+            for (i = 0; i < name.Length; i++)
             {
                 sum += (int)name[i];
             }
+
             avg = sum / (double)name.Length;
-            
-            if (Math.Round(avg) %3 == 0)
+            return avg;
+        }
+
+        private static Smells GetSmell(double avg)
+        {
+            int rounded = (int)Math.Round(avg);
+
+            if (rounded % 3 == 0)
             {
-                Console.ForegroundColor = ConsoleColor.DarkGreen;
-                Console.WriteLine("Smrdí jako cibuláči.");
+                return Smells.OnionRings;
             }
-            else if (Math.Round(avg) % 5 == 0)
+
+            if (rounded % 5 == 0)
             {
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.WriteLine($"{name} Smrdí jako koňská řiť.");
+                return Smells.HorseAss;
             }
-            else if (Math.Round(avg) % 7 == 0)
+
+            if (rounded % 7 == 0)
             {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine($"{name} Smrdí jako bolavá noha bezdomovce.");
+                return Smells.HoboToughLife;
             }
-            else 
-            {
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine($"{name} Nesmrdí vůbec.");
-            }
+
+            return Smells.NoSmell;
         }
     }
 }
---
+++ SmellLevels/SmellLevels/Texts.cs
@@ -0,0 +1,10 @@
+﻿namespace SmellLevels
+{
+    public static class Texts
+    {
+        public const string NoSmell = "nesmrdí vůbec";
+        public const string OnionRings = "smrdí jako cibuláč";
+        public const string HorseAss = "smrdí jako koňská řiť";
+        public const string HoboToughLife = "smrdí jako bolavá noha bezdomovce";
+    }
+}
