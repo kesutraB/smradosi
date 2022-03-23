26d6a3ae4fb9b2a6eb4e359a31ed5ae859fd1672
kesutraB <99909890+kesutraB@users.noreply.github.com>
2022-03-23 15:51:03 +0100

    dictionary

--- SmellLevels/SmellLevels/Program.cs
+++ SmellLevels/SmellLevels/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SmellLevels
 {
@@ -13,52 +14,34 @@ namespace SmellLevels
             NoSmell,
         }
 
+        static Dictionary<string, Smells> people = new Dictionary<string, Smells>();
+
         static void Main(string[] args)
         {
-            Dictionary<string, Smells> table = new Dictionary<string, Smells>();
-
-            string name = GetName();
-            double avg = GetAvg(name);
-            Smells level = GetSmell(avg);
-            switch (level)
+            while (true)
             {
-                case Smells.NoSmell:
-                {
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine(Texts.NoSmell);
-                }
-                    break;
-
-                case Smells.OnionRings:
-                {
-                    Console.ForegroundColor = ConsoleColor.DarkGreen;
-                    Console.WriteLine(Texts.OnionRings);
-                }
-                    break;
-
-                case Smells.HorseAss:
-                {
-                    Console.ForegroundColor = ConsoleColor.DarkYellow;
-                    Console.WriteLine(Texts.HorseAss);
-                }
-                    break;
-
-                case Smells.HoboToughLife:
-                {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine(Texts.HoboToughLife);
-                }
-                    break;
+                string name = GetName();
+                double avg = GetAvg(name);
+                Smells level = GetSmell(name,avg);
+                AddPeople(name, level);
+                Table();
+                Console.WriteLine();
+                Console.WriteLine();
             }
-            Console.ResetColor();
         }
 
         private static string GetName()
         {
             string name;
-            Console.WriteLine("Zadej jméno: ");
+            Console.WriteLine("Zadej jméno smraďocha: ");
             name = Console.ReadLine();
             return name;
+        }
+
+        private static void AddPeople(string name, Smells level)
+        {
+            Console.WriteLine($"{name} {level}.");
+            people.Add(name, level);
         }
 
         private static double GetAvg(string name)
@@ -74,13 +57,13 @@ namespace SmellLevels
             return avg;
         }
 
-        private static Smells GetSmell(double avg)
+        private static Smells GetSmell(string name, double avg)
         {
             int rounded = (int)Math.Round(avg);
 
-            if (rounded % 3 == 0)
+            if (rounded % 7 == 0 || name.ToUpper() == "JANUS RADULUS")
             {
-                return Smells.OnionRings;
+                return Smells.HoboToughLife;
             }
 
             if (rounded % 5 == 0)
@@ -88,12 +71,52 @@ namespace SmellLevels
                 return Smells.HorseAss;
             }
 
-            if (rounded % 7 == 0)
+            if (rounded % 3 == 0)
             {
-                return Smells.HoboToughLife;
+                return Smells.OnionRings;
             }
 
             return Smells.NoSmell;
+        }
+
+        private static void Table()
+        {
+            Console.WriteLine("Tabulka smraďochů:\n---------------");
+            foreach (KeyValuePair<string, Smells> person in people)
+            {
+                Console.Write($"{person.Key}\t");
+                switch (person.Value)
+                {
+                    case Smells.NoSmell:
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine(Texts.NoSmell);
+                    }
+                        break;
+
+                    case Smells.OnionRings:
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkGreen;
+                        Console.WriteLine(Texts.OnionRings);
+                    }
+                        break;
+
+                    case Smells.HorseAss:
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkYellow;
+                        Console.WriteLine(Texts.HorseAss);
+                    }
+                        break;
+
+                    case Smells.HoboToughLife:
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine(Texts.HoboToughLife);
+                    }
+                        break;
+                }
+                Console.ResetColor();
+            }
         }
     }
 }
